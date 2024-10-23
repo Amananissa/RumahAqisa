adc77d8aedaed48ebb38b161e99ba6b0791be7ee
Amandaia <Amandaia.com>
2024-10-23 10:04:28 +0700

    perubahanx

--- index.php
+++ index.php
@@ -831,13 +831,8 @@ Dengan semangat menyediakan cemilan sehat dan minuman herbal alami, Rumah Aqisa 
 
           <div class="col-lg-4 col-md-6 member" data-aos="fade-up" data-aos-delay="100">
             <div class="member-img">
-              <img src="assets/img/team/team-1.jpg" class="img-fluid" alt="">
-              <div class="social">
-                <a href="#"><i class="bi bi-twitter-x"></i></a>
-                <a href="#"><i class="bi bi-facebook"></i></a>
-                <a href="#"><i class="bi bi-instagram"></i></a>
-                <a href="#"><i class="bi bi-linkedin"></i></a>
-              </div>
+              <img src="assets/img/achievmen/achv-1.jpg" class="img-fluid" alt="">
+              
             </div>
             <div class="member-info text-center">
               <h4>Walter White</h4>
@@ -848,13 +843,8 @@ Dengan semangat menyediakan cemilan sehat dan minuman herbal alami, Rumah Aqisa 
 
           <div class="col-lg-4 col-md-6 member" data-aos="fade-up" data-aos-delay="200">
             <div class="member-img">
-              <img src="assets/img/team/team-2.jpg" class="img-fluid" alt="">
-              <div class="social">
-                <a href="#"><i class="bi bi-twitter-x"></i></a>
-                <a href="#"><i class="bi bi-facebook"></i></a>
-                <a href="#"><i class="bi bi-instagram"></i></a>
-                <a href="#"><i class="bi bi-linkedin"></i></a>
-              </div>
+              <img src="assets/img/achievmen/achv-2.jpg" class="img-fluid" alt="">
+              
             </div>
             <div class="member-info text-center">
               <h4>Sarah Jhonson</h4>
@@ -865,13 +855,8 @@ Dengan semangat menyediakan cemilan sehat dan minuman herbal alami, Rumah Aqisa 
 
           <div class="col-lg-4 col-md-6 member" data-aos="fade-up" data-aos-delay="300">
             <div class="member-img">
-              <img src="assets/img/team/team-3.jpg" class="img-fluid" alt="">
-              <div class="social">
-                <a href="#"><i class="bi bi-twitter-x"></i></a>
-                <a href="#"><i class="bi bi-facebook"></i></a>
-                <a href="#"><i class="bi bi-instagram"></i></a>
-                <a href="#"><i class="bi bi-linkedin"></i></a>
-              </div>
+              <img src="assets/img/achievmen/achv-3.jpg" class="img-fluid" alt="">
+              
             </div>
             <div class="member-info text-center">
               <h4>William Anderson</h4>
@@ -882,13 +867,8 @@ Dengan semangat menyediakan cemilan sehat dan minuman herbal alami, Rumah Aqisa 
 
           <div class="col-lg-4 col-md-6 member" data-aos="fade-up" data-aos-delay="400">
             <div class="member-img">
-              <img src="assets/img/team/team-4.jpg" class="img-fluid" alt="">
-              <div class="social">
-                <a href="#"><i class="bi bi-twitter-x"></i></a>
-                <a href="#"><i class="bi bi-facebook"></i></a>
-                <a href="#"><i class="bi bi-instagram"></i></a>
-                <a href="#"><i class="bi bi-linkedin"></i></a>
-              </div>
+              <img src="assets/img/achievmen/achv-4.jpg" class="img-fluid" alt="">
+              
             </div>
             <div class="member-info text-center">
               <h4>Amanda Jepson</h4>
@@ -899,43 +879,33 @@ Dengan semangat menyediakan cemilan sehat dan minuman herbal alami, Rumah Aqisa 
 
           <div class="col-lg-4 col-md-6 member" data-aos="fade-up" data-aos-delay="500">
             <div class="member-img">
-              <img src="assets/img/team/team-5.jpg" class="img-fluid" alt="">
-              <div class="social">
-                <a href="#"><i class="bi bi-twitter-x"></i></a>
-                <a href="#"><i class="bi bi-facebook"></i></a>
-                <a href="#"><i class="bi bi-instagram"></i></a>
-                <a href="#"><i class="bi bi-linkedin"></i></a>
-              </div>
+              <img src="assets/img/achievmen/achv-5.jpg" class="img-fluid" alt="">
+              
             </div>
             <div class="member-info text-center">
               <h4>Brian Doe</h4>
               <span>Marketing</span>
               <p>Qui consequuntur quos accusamus magnam quo est molestiae eius laboriosam sunt doloribus quia impedit laborum velit</p>
             </div>
-          </div><!-- End Team Member -->
+          </div><!-- End achiev Member -->
 
           <div class="col-lg-4 col-md-6 member" data-aos="fade-up" data-aos-delay="600">
             <div class="member-img">
-              <img src="assets/img/team/team-6.jpg" class="img-fluid" alt="">
-              <div class="social">
-                <a href="#"><i class="bi bi-twitter-x"></i></a>
-                <a href="#"><i class="bi bi-facebook"></i></a>
-                <a href="#"><i class="bi bi-instagram"></i></a>
-                <a href="#"><i class="bi bi-linkedin"></i></a>
-              </div>
+              <img src="assets/img/achievmen/achv-6.jpg" class="img-fluid" alt="">
+              
             </div>
             <div class="member-info text-center">
               <h4>Josepha Palas</h4>
               <span>Operation</span>
               <p>Sint sint eveniet explicabo amet consequatur nesciunt error enim rerum earum et omnis fugit eligendi cupiditate vel</p>
             </div>
-          </div><!-- End Team Member -->
+          </div><!-- End achievmen Member -->
 
         </div>
 
       </div>
 
-    </section><!-- /Team Section -->
+    </section><!-- /achievmen Section -->
 
     <!-- Call To Action Section -->
     <section id="call-to-action" class="call-to-action section dark-background">
